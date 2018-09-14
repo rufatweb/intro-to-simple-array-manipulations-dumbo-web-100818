251b7477608a6633e48519b0c2d01ea7bf02d7b0
github username <github email address>
2018-09-14 18:25:32 -0400

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,46 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr_one, arr_two)
+  arr_one.concat(arr_two)
+end
+
+def using_insert(arr, item)
+  arr.insert(4, item)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end
+def using_delete_at(arr, int)
+  arr.delete_at(int)
+end
